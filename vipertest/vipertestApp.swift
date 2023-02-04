766db378798197af9dd962e7905f638917b4ff92
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-02-04 14:00:24 -0300

    Initial Commit

---
+++ vipertest/vipertestApp.swift
@@ -0,0 +1,17 @@
+//
+//  vipertestApp.swift
+//  vipertest
+//
+//  Created by Jonn Alves on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct vipertestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
